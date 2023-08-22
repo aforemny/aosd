6062dc60f6620a484c3cb38de275b5addea33676
Alexander Foremny <aforemny@posteo.de>
2023-08-22 16:22:15 +0200

    add IO

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 ## Pre-requisites
 
 - xcompmgr
+
+## Usage
+
+### Volume control
+
+```console
+get_volume() {
+  while true; do
+    pamixer --get-volume
+    sleep 1s
+  done
+}
+
+set_volume() {
+  while read volume; do
+    pamixer --set-volume "$volume"
+  done
+}
+
+get_volume | aosd | set_volume
+```
--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,6 @@ import Control.Concurrent
 import Control.Monad
 import Data.Bits
 import Data.IORef
-import Foreign.Marshal
 import Graphics.X11
 import Graphics.X11.Xlib.Extras
 import System.IO
@@ -13,7 +12,6 @@ main :: IO ()
 main = do
   dpy <- openDisplay ""
   let scrn = defaultScreen dpy
-      scr = defaultScreenOfDisplay dpy
       black = blackPixel dpy scrn
       white = whitePixel dpy scrn
       root = defaultRootWindow dpy
@@ -23,74 +21,95 @@ main = do
       dpth = visualInfo_depth vinfo
       vis = visualInfo_visual vinfo
       vmsk = cWColormap .|. cWBorderPixel .|. cWBackingPixel .|. cWOverrideRedirect
+      width = fi (displayWidth dpy scrn)
+      height = 64
   cmap <- createColormap dpy root vis allocNone
   win <- allocaSetWindowAttributes $ \attr -> do
     set_colormap attr cmap
     set_border_pixel attr 0
     set_background_pixel attr 0
     set_override_redirect attr True
-    createWindow dpy root 0 0 2560 64 0 dpth cls vis vmsk attr
+    createWindow dpy root 0 0 width height 0 dpth cls vis vmsk attr
   atom <- internAtom dpy "ATOM" True
   wmState <- internAtom dpy "_NET_WM_STATE" False
   wmStateSticky <- internAtom dpy "_NET_WM_STATE_STICKY" False
   wmStateAbove <- internAtom dpy "_NET_WM_STATE_ABOVE" False
   changeProperty32 dpy win wmState atom propModeReplace $
-    [ fromIntegral wmStateAbove,
-      fromIntegral wmStateSticky
+    [ fi wmStateAbove,
+      fi wmStateSticky
     ]
   wmWindowType <- internAtom dpy "_NET_WM_WINDOW_TYPE" False
   wmWindowTypeDock <- internAtom dpy "_NET_WM_WINDOW_TYPE_DOCK" False
   changeProperty32 dpy win wmWindowType atom propModeReplace $
-    [fromIntegral wmWindowTypeDock]
+    [fi wmWindowTypeDock]
   let drw = win
   gc <- createGC dpy drw
   mapWindow dpy win
   raiseWindow dpy win
   selectInput dpy win buttonPressMask
-  xR <- newIORef 2560
+  pR <- newIORef (1 :: Float)
   grabbingR <- newIORef False
+  forkIO $ forever $ do
+    p <- (/ 100) . read <$> getLine
+    writeIORef pR p
   allocaXEvent $ \ev -> forever $ do
     hFlush stdout
-    x <- readIORef xR
+    p <- readIORef pR
     grabbing <- readIORef grabbingR
     if grabbing
       then do
-        setForeground dpy gc black
-        setBackground dpy gc black
-        fillRectangle dpy drw gc 0 0 2560 64
+        setForeground dpy gc 0
+        setBackground dpy gc 0
+        fillRectangle dpy drw gc 0 0 width height
         setForeground dpy gc white
         setBackground dpy gc white
-        fillRectangle dpy drw gc 0 0 x 64
+        fillRectangle dpy drw gc 0 0 (floor (p * fi width)) height
       else do
         setForeground dpy gc 0
         setBackground dpy gc 0
-        fillRectangle dpy drw gc 0 0 2560 64
+        fillRectangle dpy drw gc 0 0 width height
     flush dpy
-    timedOut <- waitForEvent dpy (10 ^ 3)
+    timedOut <- waitForEvent dpy 1666
     when (not timedOut) $ do
       nextEvent dpy ev
       e <- getEvent ev
-      print e
+      -- hPutStrLn stderr (show e)
       if grabbing
         then do
           case e of
-            MotionEvent {ev_x} ->
-              writeIORef xR (fromIntegral ev_x)
-            ButtonEvent {ev_event_type, ev_button, ev_x, ev_time} ->
+            MotionEvent {ev_x} -> do
+              let p = fi ev_x / fi width
+              writeIORef pR p
+              -- TODO debounce duplicate values
+              putStrLn (show (floor (p * 100)))
+            ButtonEvent {ev_event_type, ev_button, ev_x} ->
               when (ev_event_type == buttonRelease && ev_button == button1) $ do
-                ungrabPointer dpy ev_time
-                writeIORef xR (fromIntegral ev_x)
+                ungrabPointer dpy currentTime
+                let p = fi ev_x / fi width
+                writeIORef pR p
+                -- TODO debounce duplicate values
+                putStrLn (show (floor (p * 100)))
                 writeIORef grabbingR False
+                -- XXX since -threaded, sync is required or else the application locks up
+                sync dpy True
             _ -> pure ()
         else do
           case e of
-            ButtonEvent {ev_event_type, ev_button, ev_x, ev_time} ->
+            ButtonEvent {ev_event_type, ev_button, ev_x} ->
               when (ev_event_type == buttonPress && ev_button == button1) $ do
-                grabStatus <- grabPointer dpy win True (pointerMotionMask .|. buttonReleaseMask) grabModeAsync grabModeAsync 0 0 ev_time
+                grabStatus <- grabPointer dpy win True (pointerMotionMask .|. buttonReleaseMask) grabModeAsync grabModeAsync none none currentTime
                 when (grabStatus == grabSuccess) $ do
-                  writeIORef xR (fromIntegral ev_x)
+                  let p = fi ev_x / fi width
+                  writeIORef pR p
+                  -- TODO debounce duplicate values
+                  putStrLn (show (floor (p * 100)))
                   writeIORef grabbingR True
+                  -- XXX since -threaded, sync is required or else the application locks up
+                  sync dpy True
                 pure ()
             _ -> pure ()
   destroyWindow dpy win
   return ()
+
+fi :: (Integral a, Num b) => a -> b
+fi = fromIntegral
