983b0e0c1cede5ea1740538651b9b463fdbc4880
Alexander Foremny <aforemny@posteo.de>
2023-08-23 12:36:28 +0200

    separate event processing and drawing

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -8,9 +9,9 @@ import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad
 import Data.Bits
+import GHC.Ptr (Ptr)
 import Graphics.X11 qualified as X
 import Graphics.X11.Xlib.Extras qualified as X
-import System.IO
 import System.IO.Error
 
 data Env = Env
@@ -26,63 +27,73 @@ data Env = Env
 
 data State = State
   { p :: Float,
-    grabbing :: Bool
+    grabbing :: Bool,
+    dirty :: Bool
   }
+  deriving (Show, Eq)
 
 main :: IO ()
 main = bracket createWindow destroyWindow run
 
 run :: (Env, TVar State) -> IO ()
-run (Env {..}, stateT) = do
+run (env@(Env {..}), stateT) = do
   X.allocaXEvent $ \ev -> forever $ do
-    hFlush stdout
-    State {..} <- atomically $ readTVar stateT
+    state <- atomically $ readTVar stateT
+    state' <- processEvents ev env state
     -- XXX note that _NET_WM_STATE_FULLSCREEN should be enough to stack this above _NET_WM_TYPE_DOCK, but it isn't
     X.raiseWindow dpy win
-    if grabbing
-      then do
-        X.setForeground dpy gc 0
-        X.setBackground dpy gc 0
-        X.fillRectangle dpy drw gc 0 0 (fi width) (fi height)
-        X.setForeground dpy gc white
-        X.setBackground dpy gc white
-        X.fillRectangle dpy drw gc 0 0 (floor (p * fi width)) (fi height)
-      else do
-        X.setForeground dpy gc 0
-        X.setBackground dpy gc 0
-        X.fillRectangle dpy drw gc 0 0 (fi width) (fi height)
-    X.flush dpy
-    timedOut <- X.waitForEvent dpy 1666
-    when (not timedOut) $ do
-      X.nextEvent dpy ev
-      e <- X.getEvent ev
-      -- hPutStrLn stderr (show e)
-      if grabbing
-        then do
-          case e of
-            X.MotionEvent {X.ev_x} -> do
+    when (state' /= state) $ paint env state'
+    X.sync dpy False
+    atomically $ writeTVar stateT state' {dirty = False}
+    threadDelay 1666
+
+processEvents :: Ptr X.XEvent -> Env -> State -> IO State
+processEvents ev env@(Env {..}) state = do
+  n <- X.eventsQueued dpy X.queuedAlready
+  foldM (\state _ -> processEvent ev env state) state [1 .. n]
+
+processEvent :: Ptr X.XEvent -> Env -> State -> IO State
+processEvent ev (Env {..}) state@(State {..}) = do
+  X.nextEvent dpy ev
+  e <- X.getEvent ev
+  if
+      | grabbing,
+        X.MotionEvent {X.ev_x} <- e -> do
+          let p = fi ev_x / fi width
+          pure state {p = p}
+      | grabbing,
+        X.ButtonEvent {X.ev_event_type, X.ev_button, X.ev_x} <- e,
+        ev_event_type == X.buttonRelease,
+        ev_button == X.button1 -> do
+          X.ungrabPointer dpy X.currentTime
+          let p = fi ev_x / fi width
+          pure state {p = p, grabbing = False}
+      | not grabbing,
+        X.ButtonEvent {X.ev_event_type, X.ev_button, X.ev_x} <- e,
+        ev_event_type == X.buttonPress,
+        ev_button == X.button1 -> do
+          grabStatus <- X.grabPointer dpy win True (X.pointerMotionMask .|. X.buttonReleaseMask) X.grabModeAsync X.grabModeAsync X.none X.none X.currentTime
+          if (grabStatus == X.grabSuccess)
+            then do
               let p = fi ev_x / fi width
-              atomically $ modifyTVar stateT (\state -> state {p = p})
-            X.ButtonEvent {X.ev_event_type, X.ev_button, X.ev_x} ->
-              when (ev_event_type == X.buttonRelease && ev_button == X.button1) $ do
-                X.ungrabPointer dpy X.currentTime
-                let p = fi ev_x / fi width
-                atomically $ modifyTVar stateT (\state -> state {p = p, grabbing = False})
-                -- XXX since -threaded, sync is required or else the application locks up
-                X.sync dpy True
-            _ -> pure ()
-        else do
-          case e of
-            X.ButtonEvent {X.ev_event_type, X.ev_button, X.ev_x} ->
-              when (ev_event_type == X.buttonPress && ev_button == X.button1) $ do
-                grabStatus <- X.grabPointer dpy win True (X.pointerMotionMask .|. X.buttonReleaseMask) X.grabModeAsync X.grabModeAsync X.none X.none X.currentTime
-                when (grabStatus == X.grabSuccess) $ do
-                  let p = fi ev_x / fi width
-                  atomically $ modifyTVar stateT (\state -> state {p = p, grabbing = True})
-                  -- XXX since -threaded, sync is required or else the application locks up
-                  X.sync dpy True
-                pure ()
-            _ -> pure ()
+              pure state {p = p, grabbing = True}
+            else pure state
+      | X.ExposeEvent {} <- e ->
+          pure state {dirty = True}
+      | otherwise -> pure state
+
+paint :: Env -> State -> IO ()
+paint Env {..} State {grabbing = False} = do
+  X.setForeground dpy gc 0
+  X.setBackground dpy gc 0
+  X.fillRectangle dpy drw gc 0 0 (fi width) (fi height)
+paint Env {..} State {grabbing = True, ..} = do
+  X.setForeground dpy gc 0
+  X.setBackground dpy gc 0
+  X.fillRectangle dpy drw gc 0 0 (fi width) (fi height)
+  X.setForeground dpy gc white
+  X.setBackground dpy gc white
+  X.fillRectangle dpy drw gc 0 0 (floor (p * fi width)) (fi height)
 
 destroyWindow :: (Env, TVar State) -> IO ()
 destroyWindow (Env {..}, _) = do
@@ -123,9 +134,10 @@ createWindow = do
   let drw = win
   gc <- X.createGC dpy drw
   X.mapWindow dpy win
-  X.selectInput dpy win X.buttonPressMask
+  X.selectInput dpy win (X.buttonPressMask .|. X.exposureMask)
   let p = 1
       grabbing = False
+      dirty = True
   stateT <- newTVarIO State {..}
   _ <- forkIO $ consumeInput stateT
   _ <- forkIO $ produceOutput stateT
@@ -145,9 +157,7 @@ consumeInput stateT =
     (\_ -> pure ())
 
 produceOutput :: TVar State -> IO ()
-produceOutput stateT = do
-  state <- atomically $ readTVar stateT
-  go Nothing
+produceOutput stateT = go Nothing
   where
     go o' = do
       o <- atomically $ do
@@ -158,7 +168,7 @@ produceOutput stateT = do
       putStrLn o
       go (Just o)
     toOutput p =
-      show (floor (100 * p))
+      show (floor (100 * p) :: Int)
 
 fi :: (Integral a, Num b) => a -> b
 fi = fromIntegral
